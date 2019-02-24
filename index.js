30db883cd83855db75ff11e565e0ed7f1328a098
Marco Esposito <marco.esposito@spindox.it>
2019-02-24 11:16:06 +0100

    api prefix set up

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3001;
+const apiPrefix = process.env.API_PREFIX;
 
-app.get("/api/v1", (req, res) => {
+app.get(apiPrefix, (req, res) => {
   return res.send("Hello World!");
 });
 
